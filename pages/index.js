dbdb056959905ac6dadae8ee0a1b406ec97d400e
surender singh <surendersingh9728511667@gmail.com>
2024-03-04 11:29:44 +0530

    add footer

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Services from "@/components/Services";
+import Footer from "../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
    <>
-   <Services/></>
+   <Services/>
+   <Footer/>
+   </>
   );
 }
